51eed2760f57049040b7711c05a8a28680cefc3a
Guilherme Alves <guilhermeari@outlook.com>
2021-08-27 22:30:26 -0300

    Config dotenv

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-require('dotenv').config(); //initialize dotenv
+require('dotenv/config');
+//initialize dotenv
 const Discord = require('discord.js'); //import discord.js
 
-const client = new Discord.Client(); //create new client
+const client = new Discord.Client({ intents: ['GUILDS', 'GUILD_MESSAGES']}); //create new client
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
 //make sure this line is the last line
-console.log(process.env.CLIENT_TOKEN);
-client.login("5c0cbd9e693f20749b559d3bb7c2b489c3a0232fdc6545bde92be604d6879928"); //login bot using token
+
+client.login(process.env.CLIENT_TOKEN); //login bot using token
