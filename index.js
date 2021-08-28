3ba6961064f5a910683c96b2b1f198daa50fef9b
Welber Serafim <serafimwelber@gmail.com>
2021-08-27 22:34:21 -0300

    mudança no index

--- index.js
+++ index.js
@@ -8,5 +8,5 @@ client.on('ready', () => {
 });
 
 //make sure this line is the last line
-console.log(process.env.CLIENT_TOKEN);
-client.login("5c0cbd9e693f20749b559d3bb7c2b489c3a0232fdc6545bde92be604d6879928"); //login bot using token
+
+client.login(""); //login bot using token
